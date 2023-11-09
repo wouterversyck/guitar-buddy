4b758f88311ab6912f080792ccf6704dabde786c
Wouter Versyck <wouter.versyck@gmail.com>
2023-11-09 17:48:20 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Scale, Mode } from "tonal";
+import "./App.css";
+import { keys } from "./helper-functions";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 function App() {
+  const [key, setKey] = React.useState(keys[0]);
+
+  const handleChange = (event) => {
+    setKey(event.target.value);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={key}
+              onChange={handleChange}
+            >
+              {keys.map(e => <MenuItem value={e}>{e}</MenuItem>)}
+            </Select>
+          </FormControl>
+          {Scale.get(key + " major").notes
+              .map(e => <span>{e}</span>)}
+          <hr/>
+          {Mode.notes("dorian", "C").map(e => <span>{e.replace("b", "♭")} - </span>)}
+        </div>
       </header>
     </div>
   );
